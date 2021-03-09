a582bdcb3817afbdd955cadde9203621e3316a1f
Exosunandnet <you@example.com>
2021-03-09 23:35:11 +0100

    Added lsd and bat packages in setup.sh

--- setup.sh
+++ setup.sh
@@ -57,7 +57,7 @@ _pkgs=(bc bmon calc calcurse curl dbus desktop-file-utils elinks feh fontconfig-
 		geany git gtk2 gtk3 htop-legacy imagemagick jq leafpad man mpc mpd mutt ncmpcpp \
 		ncurses-utils neofetch netsurf obconf openbox openssl-tool polybar ranger rofi \
 		startup-notification termux-api thunar tigervnc vim wget xarchiver xbitmaps xcompmgr \
-		xfce4-settings xfce4-terminal xmlstarlet xorg-font-util xorg-xrdb zsh)
+		xfce4-settings xfce4-terminal xmlstarlet xorg-font-util xorg-xrdb zsh lsd bat)
 
 setup_base() {
 	echo -e ${RED}"\n[*] Installing Termux Desktop..."
@@ -120,11 +120,13 @@ setup_omz() {
 	# Append some aliases
 	cat >> $HOME/.zshrc <<- _EOF_
 		#------------------------------------------
+		alias ls='lsd'
 		alias l='ls -lh'
 		alias ll='ls -lah'
 		alias la='ls -a'
 		alias ld='ls -lhd'
 		alias p='pwd'
+		alias cat='bat'
 
 		#alias rm='rm -rf'
 		alias u='cd $PREFIX'
